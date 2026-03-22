cdc2c9f1583c9469a9059348088351163fdad617
Ahad-Channa <ahadchanna45@gmail.com>
2026-03-21 22:53:01 -0700

    setting veriable for deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (user) {
-            const socketInstance = io(import.meta.env.VITE_API_BASE_URL || "http://localhost:5000", {
+            const socketInstance = io(import.meta.env.VITE_API_URL || "http://localhost:5000", {
                 query: {
                     userId: user._id,
                 },
