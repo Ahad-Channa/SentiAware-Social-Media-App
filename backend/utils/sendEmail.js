be27ac61e52c59e9849d157106409811b41ee984
Ahad-Channa <ahadchanna45@gmail.com>
2026-03-22 00:11:26 -0700

    Up

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -6,9 +6,7 @@ dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (to, subject, text, html = null) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
