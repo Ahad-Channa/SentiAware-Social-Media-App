088d9438dfb424304312f9ffa34b791bda9c1633
Ahad-Channa <ahadchanna45@gmail.com>
2026-03-21 23:42:37 -0700

    updating Ipv4

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,4 +1,8 @@
 import nodemailer from "nodemailer";
+import dns from "dns";
+
+// Force IPv4 because Render doesn't support outgoing IPv6 to Gmail
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (to, subject, text, html = null) => {
   const transporter = nodemailer.createTransport({
